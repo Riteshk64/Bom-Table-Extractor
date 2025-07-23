af256965afa7ee2df0623553962e6902573ab24a
Ritesh Pramod Kumar <121807@tce.co.in>
2025-07-23 14:42:55 +0530

    First OCR code.

---
+++ main.py
@@ -0,0 +1,17 @@
+import OcrToTableTool as ottt
+import TableExtractor as te
+import TableLinesRemover as tlr
+import cv2
+
+path_to_image = "drawing.jpg"
+table_extractor = te.TableExtractor(path_to_image)
+perspective_corrected_image = table_extractor.execute()
+
+lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
+image_without_lines = lines_remover.execute()
+
+ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
+ocr_tool.execute()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
